848298126c3c1802804cc0c08d93212afdae08be
root <you@example.com>
2019-04-20 17:01:42 +0530

    newfix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,11 @@ public class Greeter {
 
   }
 
-  public final String greet(final String someone) {
+ /**
+  * @param someone name of a person
+  * @return greeting string
+  */
+    public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
